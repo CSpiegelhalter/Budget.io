15d98686a4eb5d07e6d3bdcae58fc72d0eca91d5
Curt Spiegelhalter <i16dcurt@gmail.com>
2021-03-19 12:51:12 -0500

    Fix for heroku deployment

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fast-brushlands-99096", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
